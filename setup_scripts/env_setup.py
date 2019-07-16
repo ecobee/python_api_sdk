42801b032cf67b85edd7636bbbaf456042ebd146
Benjamin Mitchell <ben.m@ecobee.com>
2019-07-16 13:19:57 -0400

    Fixed bug with setup script

--- setup_scripts/env_setup.py
+++ setup_scripts/env_setup.py
@@ -3,52 +3,64 @@
 import os
 
 
+def setup():
 
-class Setup:
-    
-    def __init__(self, print_mode):
-        self.print_mode = print_mode
-        self.pref_file = self.get_preferences_location()
+    print_mode = False
+    HOME_DIR = os.path.expanduser("~")
+    EBAPI_DIR = os.path.join(HOME_DIR, ".ebapi")
 
-    def get_preferences_location(self)
-        default_directory = os.path.expanduser("~")
-        preferences_file_name = ".ebapi_preferences"
-        default_save_file = os.path.join(home, preferences_file_name)
-        save_dir = input_path("Where should we save your preferences?\n" \
-                              "Enter nothing to use default: {}".format(default_save_file))
-        if save_dir == "":
-            return default_save_file
+    if not os.path.exists(EBAPI_DIR):
+        os.mkdir(EBAPI_DIR)
+
+    api_key = input("Enter you Ecobee Api Application Key: ")
+
+    evs = {
+        "EBAPI_USER_TOKENS_FILE": os.path.join(EBAPI_DIR, "tstat.csv"),
+        "EBAPI_USER_TSTAT_FILE": os.path.join(EBAPI_DIR, "user.csv"),
+        "ECOBEE_APPLICATION_KEY": api_key
+    }
+
+    lines = ["#Created by EBAPI Setup stript\n"]
+    lines.extend([format_ev(key, val) for key, val in evs.items()])
+
+    prefrences_file = os.path.join(EBAPI_DIR, "prefrences.sh")
+
+    for line in lines:
+        print_or_record(prefrences_file, line, print_mode)
+
+    lines = [
+        "#Added by the EBAPI setup script\n",
+        "source {}\n".format(prefrences_file)
+    ]
+
+    bash_profile = os.path.join(HOME_DIR, ".bash_profile")
+    for line in lines:
+        print_or_record(bash_profile, line, print_mode)
+
+
+def print_or_record(save_file, string, print_mode):
+    if print_mode:
+        print("would write {} to {}".format(string, save_file))
+    else:
+        with open(save_file, 'a') as out_file:
+            out_file.write(string)
+
+
+def format_ev(key, value):
+    return 'export {}="{}"\n'.format(key, value)
+
+
+def input_path(message, default=None):
+    while True:
+        path = input(message)
+        if path == "" and default is not None:
+            print("Using default.")
+            return path
+        elif os.path.exists(path):
+            return path
         else:
-            return os.path.join(save_dir, preferences_file_name)
+            print("Error supplied path does not exist please try again.")
 
-    def get_api_key(self):
-        api_key = input("Enter you Ecobee Api Application Key: ")
-        self.store_key_value(self.pref_file, "ECOBEE_APPLICATION_KEY", api_key)
 
-    def get_local_creds_directory(self):
-        path = input_path("Where should we save our ")
-
-    def input_path(self, message, default=None):
-        while True:
-            path = input(message)
-            if path == "" and default is not None:
-                print("Using default.")
-                return path
-            elif os.path.exists(path):
-                return path
-            else:
-                print("Error supplied path does not exist please try again.")
-    def store_key_value(self, save_file, key, value):
-        line = 'export {}="{}"\n'.format(key, value)
-        try:
-            self.lines[save_file].append(
-        except KeyError:
-            self.lines[save_file] = [line]
-
-    def print_or_record(self, save_file, string):
-        if self.print_mode:
-            print("would write {} to {}".format(string, save_file))
-        else:
-            with open(save_file, 'w') as out_file:
-                outfile.write(string)
-            
+if __name__ == "__main__":
+    setup()
